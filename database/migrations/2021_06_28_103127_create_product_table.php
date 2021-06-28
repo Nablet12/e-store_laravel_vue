2d96f7d05572cac566730ee659cd0ce02d1ced57
Nablet12 <nableta12@gmail.com>
2021-06-28 13:47:43 +0300

    commit "migration files"

---
+++ database/migrations/2021_06_28_103127_create_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('Title');
+            $table->boolean('Availability');
+            $table->string('Category');
+            $table->double('Sale Price');
+            $table->double('Bought Price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
